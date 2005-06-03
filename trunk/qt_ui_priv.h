d08c0ed7540011d202400bec63d773ebf17e78c1
flynn <flynn@e87d916d-a6f8-0310-bef8-b6030da855e1>
2005-06-03 17:34:56 +0000

    pre-moc class definition

---
+++ trunk/qt_ui_priv.h
@@ -0,0 +1,20 @@
+#ifndef QT_UI_PRIV_H
+#define QT_UI_PRIV_H
+
+#include <qobject.h>
+
+class MiscDialogs : public QObject {
+    Q_OBJECT
+
+public:
+
+
+public slots:
+
+    void CheckSaved();
+
+
+};
+
+
+#endif
