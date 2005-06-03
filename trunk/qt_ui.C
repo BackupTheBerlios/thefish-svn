e3a5a79220d2576fa942c8b0ef221ffec7c57641
flynn <flynn@e87d916d-a6f8-0310-bef8-b6030da855e1>
2005-06-03 17:35:08 +0000

    Fix compile on -STABLE

--- trunk/qt_ui.C
+++ trunk/qt_ui.C
@@ -47,8 +47,18 @@
 #include <qtabwidget.h>
 #include <qwidget.h>
 
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+#include <time.h>
+extern char *tzname[2];
+
 
 #include "parser.h"
 #include "thefish.h"
